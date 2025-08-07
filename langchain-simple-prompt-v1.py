36916e9970b614a0cdf67aa52d55ac5976b4531a
Kumarpal Nagar <49683369+cyber-ai-warrior@users.noreply.github.com>
2025-08-07 18:10:37 +0530

    add v2 of prompt example.

---
+++ langchain-simple-prompt-v1.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain_core.prompts import PromptTemplate
+
+load_dotenv()
+
+MODEL=os.getenv("MODEL")
+GROQ_API_KEY=os.getenv("GROQ_API_KEY")
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
+
+formal_template = "Five me {number_of_question} question on topic: {topic}"
+prompt = PromptTemplate.from_template(formal_template)
+model = init_chat_model(MODEL, model_provider="groq")
+chain = prompt | model
+
+number_of_question = 5
+topic = "Vimal pan masala"
+input_data = {"number_of_question": number_of_question, "topic": topic}
+
+result = chain.invoke(input=input_data)
+print(result.content)
