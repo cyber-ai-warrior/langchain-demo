1b6140dcc85a109e7ff90dc481681fd85fe69b60
Kumarpal Nagar <49683369+cyber-ai-warrior@users.noreply.github.com>
2025-08-08 12:49:50 +0530

    json parser example.

---
+++ langchain-json-parser.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import SystemMessage, HumanMessage
+
+
+load_dotenv()
+
+MODEL=os.getenv("MODEL")
+GROQ_API_KEY=os.getenv("GROQ_API_KEY")
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
+
+prompt = ChatPromptTemplate([
+    SystemMessage(content="You are teacher and you are expert to create questions for different level of grade."),
+    MessagesPlaceholder("user_input")
+])
+
+parser = JsonOutputParser()
+
+model = init_chat_model(MODEL, model_provider="groq")
+chain = prompt | model | parser
+
+question = ("Give me 3 questions of physics for 5 grade."
+            "In the format of json list of questions and each question have key 'number' and 'question'. "
+            "Output must be a list. nothing else")
+input_data = {"user_input": [HumanMessage(content=question)]}
+result = chain.invoke(input_data)
+print(result)
