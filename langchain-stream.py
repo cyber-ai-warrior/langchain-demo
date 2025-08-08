0ff702cd5cb0410773b13868e2c4b1608d655eec
Kumarpal Nagar <49683369+cyber-ai-warrior@users.noreply.github.com>
2025-08-08 11:15:15 +0530

    stream demo.

---
+++ langchain-stream.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import SystemMessage, HumanMessage
+
+
+load_dotenv()
+
+MODEL=os.getenv("MODEL")
+GROQ_API_KEY=os.getenv("GROQ_API_KEY")
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
+
+prompt = ChatPromptTemplate([
+    SystemMessage(content="You are teacher and you are expert to create questions for different level of grade."),
+    MessagesPlaceholder("user_input")
+])
+
+
+model = init_chat_model(MODEL, model_provider="groq")
+chain = prompt | model
+
+question = "Give me 3 questions of physics for 5 grade"
+input_data = {"user_input": [HumanMessage(content=question)]}
+
+chunks = []
+for chunk in chain.stream(input_data):
+    chunks.append(chunk.content)
+    print(chunk.content, end="|", flush=True)
