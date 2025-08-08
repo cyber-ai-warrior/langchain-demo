b96e7e9cd3c3f5e8eb97c54ba856e81fbd90034e
Kumarpal Nagar <49683369+cyber-ai-warrior@users.noreply.github.com>
2025-08-08 14:27:26 +0530

    add code.

--- langchain-stream-faiss.py
+++ langchain-stream-faiss.py
@@ -1,15 +1,43 @@
 import os
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
+from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.runnables import RunnablePassthrough
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 load_dotenv()
 
 MODEL=os.getenv("MODEL")
 GROQ_API_KEY=os.getenv("GROQ_API_KEY")
-
-
 os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
 model = init_chat_model(MODEL, model_provider="groq")
 
-result = model.invoke("Five me 5 question of LLM")
-print(result.content)
+
+embedding = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+vectorstore = FAISS.from_texts(
+    ["harrison worked at kensho", "harrison likes spicy food"],
+    embedding=embedding,
+)
+retriever = vectorstore.as_retriever()
+
+
+template = """Answer the question based only on the following context:
+{context}
+
+Question: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+rag_chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | prompt
+    | model
+    | StrOutputParser()
+)
+
+
+for doc in rag_chain.stream("where did harrison work?"):
+    print(doc)
